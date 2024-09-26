c584e2d236bfc2a6f48935ce544eb1375f0720a1
Tobias Rothe <tobiasrothe77@gmail.com>
2024-09-26 09:45:32 +0200

    added Funktion

--- js/script.js
+++ js/script.js
@@ -7,18 +7,23 @@ const customOptions = {
   cacheImages: true,
 };
 const P = new Pokedex.Pokedex(customOptions);
-
-async function getPokemonByName(name) {
-  const pokemon = await P.getPokemonByName(name);
-  // console.log(pokemon);
-}
-
-async function getPokedexByName(region) {
-  const pokedex = await P.getPokedexByName(region);
-  for (let i = 0; i < pokedex.pokemon_entries.length; i++) {
-    let pokemonEntry = pokedex.pokemon_entries[i];
-    let pokemonName = pokemonEntry.pokemon_species.name;
-    let pokemonType = pokemonEntry;
-    console.log(pokemonEntry);
+let pokemonData = [];
+async function loadPokemonOnTheirId(start, end) {
+  for (let i = 0; start <= end; i++) {
+    let pokemon = await P.getPokemonByName(i);
+    pokemonData.push(pokemon);
   }
 }
+
+async function loadKantoPokemon() {
+  await loadPokemonOnTheirId(1, 151);
+  console.log(pokemonData);
+}
+// async function getPokedexByName(region) {
+//   const pokedex = await P.getPokedexByName(region);
+//   for (let i = 0; i < pokedex.pokemon_entries.length; i++) {
+//     let pokemonEntry = pokedex.pokemon_entries[i];
+//     let pokemonName = pokemonEntry.pokemon_species.name;
+//     let pokemonType = pokemonEntry;
+//   }
+// }
