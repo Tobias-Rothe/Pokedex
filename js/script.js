58ed030705ba2c9a553fac2e50ffbf899cd18b84
Tobias Rothe <tobiasrothe77@gmail.com>
2024-09-26 13:31:46 +0200

    added pokecards

--- js/script.js
+++ js/script.js
@@ -8,17 +8,32 @@ const customOptions = {
 };
 const P = new Pokedex.Pokedex(customOptions);
 let pokemonData = [];
+
 async function loadPokemonOnTheirId(start, end) {
-  for (let i = 0; start <= end; i++) {
+  for (let i = start; i <= end; i++) {
     let pokemon = await P.getPokemonByName(i);
     pokemonData.push(pokemon);
   }
 }
+function renderPokemonCardSmall() {
+  let pokeRef = document.getElementById("content");
+  pokeRef.innerHTML = "";
+  for (let index = 0; index < pokemonData.length; index++) {
+    const pokeIndex = pokemonData[index];
+    console.log(pokeIndex);
 
+    pokeRef.innerHTML += getCardTemplate(pokeIndex);
+  }
+}
 async function loadKantoPokemon() {
   await loadPokemonOnTheirId(1, 151);
-  console.log(pokemonData);
 }
+
+async function init() {
+  await loadKantoPokemon();
+  renderPokemonCardSmall();
+}
+
 // async function getPokedexByName(region) {
 //   const pokedex = await P.getPokedexByName(region);
 //   for (let i = 0; i < pokedex.pokemon_entries.length; i++) {
--- js/template.js
+++ js/template.js
@@ -1,9 +1,17 @@
-function getCardTemplate(pokemon) {
-  /*html*/ `
+function getCardTemplate(pokeIndex) {
+  return /*html*/ `
         <div class="pokemon-card">
-        <span>Name: ${pokemon.name}</span>
-        <span>Typ:</span>
-        <span>KP:</span>
+        <span>Name: ${pokeIndex.name}</span>
+        <span>Typ:${
+          pokeIndex.types.length > 1
+            ? `${pokeIndex.types[0].type.name} / ${pokeIndex.types[1].type.name}`
+            : pokeIndex.types[0].type.name
+        } </span>
+        
+        <span>KP: ${pokeIndex.stats[0].base_stat}</span>
+        <div ><img class="sprite" src="${
+          pokeIndex.sprites.other["official-artwork"].front_shiny
+        }" alt=""></div>
       </div>
     `;
 }
