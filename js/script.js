9542df95a04663582fb9bff5f13f03dd8a32c401
Tobias Rothe <tobiasrothe77@gmail.com>
2024-10-01 11:34:36 +0200

    render more Pokemons

--- js/script.js
+++ js/script.js
@@ -7,35 +7,111 @@ const customOptions = {
   cacheImages: true,
 };
 
+let endlimit = 10;
+
+function showLoadingSpinner() {
+  document.getElementById("dialog-loding-spinner").classList.remove("d-none");
+  document.getElementById("content").classList.add("d-none");
+}
+function hideLoadingSpinner() {
+  document.getElementById("dialog-loding-spinner").classList.add("d-none");
+  document.getElementById("content").classList.remove("d-none");
+}
 const P = new Pokedex.Pokedex(customOptions);
 let pokemonData = [];
+let pokemonSpeciesArray = [];
+console.log(pokemonData);
 
-async function loadPokemonOnTheirId(start, end) {
-  for (let i = start; i <= end; i++) {
+async function loadPokemonOnTheirId(end) {
+  pokemonData = [];
+  for (let i = 1; i <= end; i++) {
     let pokemon = await P.getPokemonByName(i);
     pokemonData.push(pokemon);
   }
 }
+async function loadPokemonSpeciesOnID(start, end) {
+  for (let i = start; i <= end; i++) {
+    let pokemonSpecie = await P.getPokemonSpeciesByName(i);
+    pokemonSpeciesArray.push(pokemonSpecie);
+  }
+}
+let habitats = [];
 
-function renderPokemonCardSmall() {
-  let pokeRef = document.getElementById("content");
-  pokeRef.innerHTML = "";
-  for (let index = 0; index < pokemonData.length; index++) {
-    const pokeIndex = pokemonData[index];
-    console.log(pokeIndex.moves[0].move.name);
-
-    pokeRef.innerHTML += getCardTemplate(pokeIndex);
+async function getHabitats() {
+  for (let index = 0; index < pokemonSpeciesArray.length; index++) {
+    let habitat = pokemonSpeciesArray[index].habitat.name;
+    if (!habitats.includes(habitat)) {
+      habitats.push(habitat);
+    }
   }
 }
 
+async function renderPokemonCardSmall() {
+  let pokeRef = document.getElementById("content");
+  pokeRef.innerHTML = "";
+
+  for (let index = 0; index < pokemonData.length; index++) {
+    const pokeIndex = pokemonData[index];
+
+    pokeRef.innerHTML += getCardTemplate(pokeIndex, index);
+  }
+}
+
+async function renderMorePokemons() {
+  showLoadingSpinner();
+
+  endlimit += 10;
+  await loadKantoPokemon();
+  renderPokemonCardSmall();
+  hideLoadingSpinner();
+}
+
 async function loadKantoPokemon() {
-  await loadPokemonOnTheirId(1, 151);
+  await loadPokemonOnTheirId(endlimit);
+  await loadPokemonSpeciesOnID(1, 151);
 }
 
 async function init() {
+  showLoadingSpinner();
   await loadKantoPokemon();
   renderPokemonCardSmall();
+  hideLoadingSpinner();
   addHoverEffect();
+  getHabitats();
+  endlimit += 10;
+}
+
+function searchAndShowPokemon() {
+  let searchInput = document.getElementById("inputfield").value.toLowerCase();
+  if (searchInput.length > 2) {
+    document.getElementById("content").innerHTML = "";
+    for (let index = 0; index < pokemonData.length; index++) {
+      if (pokemonData[index].name.startsWith(searchInput)) {
+        document.getElementById("content").innerHTML +=
+          renderSingleSmallCard(index);
+      }
+    }
+  } else {
+    renderPokemonCardSmall();
+  }
+}
+
+function renderSingleSmallCard(index) {
+  const pokeIndex = pokemonData[index];
+  let smallcard = getCardTemplate(pokeIndex, index);
+  return smallcard;
+}
+
+function showLargeCard(index) {
+  let largeCardRef = document.getElementById("largecard");
+  largeCardRef.classList.remove("hidden");
+
+  largeCardRef.innerHTML = getLargeCardTemplate(index);
+  console.log(index);
+}
+function closeLargeCard() {
+  let largeCardRef = document.getElementById("largecard");
+  largeCardRef.classList.add("hidden");
 }
 
 function addHoverEffect() {
@@ -45,9 +121,10 @@ function addHoverEffect() {
       const rect = card.getBoundingClientRect();
       const x = (e.clientX - rect.left) / rect.width;
       const y = (e.clientY - rect.top) / rect.height;
-
       card.style.setProperty("--mouse-x", (x - 0.5).toString());
       card.style.setProperty("--mouse-y", (y - 0.5).toString());
     });
   });
 }
+
+console.log(habitats);
--- js/template.js
+++ js/template.js
@@ -1,12 +1,12 @@
-function getCardTemplate(pokeIndex) {
+function getCardTemplate(pokeIndex, index) {
   return /*html*/ `
-      <div class="pokemon-card-container">
-      <div class="pokemon-card background-${pokeIndex.types[0].type.name}">
+      <div class="pokemon-card-container" onclick="showLargeCard(${index})">
+      <div class="pokemon-card background">
         <div class="card-header">
-          <h2 class="pokemon-name"> ${pokeIndex.name}</h2>
+          <h2 class="pokemon-name"> ${pokeIndex.name} #${pokeIndex.id}</h2>
           <span class="pokemon-hp"> HP:${pokeIndex.stats[0].base_stat}</span>
         </div>
-        <div class="card-image ">
+        <div class="card-image ${pokeIndex.types[0].type.name} ">
         <img  src="${
           pokeIndex.sprites.other["official-artwork"].front_shiny
         }" alt="">
@@ -29,4 +29,19 @@ function getCardTemplate(pokeIndex) {
       </div>
           </div>
     `;
+}
+
+function getLargeCardTemplate(index) {
+  return /*html*/ `
+     <h2> ${pokemonData[index].name} #${pokemonData[index].id}</h2>
+      <img  src="${pokemonData[index].sprites.other["home"].front_default}"/>
+      <p>Typ: ${
+        pokemonData[index].types.length > 1
+          ? `${pokemonData[index].types[0].type.name} / ${pokemonData[index].types[1].type.name}`
+          : pokemonData[index].types[0].type.name
+      }</p>
+      <p>HP:${pokemonData[index].stats[0].base_stat}</p>
+      <p>Attacken:${pokemonData[index].moves[0].move.name}</p>
+      <p><button onclick="closeLargeCard()">Schließen</button></p>
+  `;
 }
